afddac3c240b8adfc1451bd57b3789d707b6e515
shrikrishnaholla <shrikrishna.holla@gmail.com>
2012-10-22 11:56:13 +0530

    Read ruby source and print each with line numbers

---
+++ rubylexer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Parse the source Ruby program and analyze its lexical correctness')
+parser.add_argument(
+    '--source', metavar='str', nargs='+', type=str,
+    help='A source to analyze')
+
+args = parser.parse_args()
+for filename in args.source:
+    sourcefile = filename
+    print 'Ruby source file:', sourcefile
+    source = file(sourcefile,'r')
+    ln = 1
+    while True:
+    	line = source.readline()
+    	if not line: break
+    	print 'line', ln, ':', line
+    source.close()
