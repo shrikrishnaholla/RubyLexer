74ddd0f1f18a952f102e110616df2b0c93497c17
shrikrishnaholla <shrikrishna.holla@gmail.com>
2012-10-24 07:14:11 +0530

    Some extension checkings

--- rubylexer.py
+++ rubylexer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import argparse
+import re
 
 parser = argparse.ArgumentParser(
     description='Parse the source Ruby program and analyze its lexical correctness')
@@ -9,14 +10,19 @@ parser.add_argument(
 
 args = parser.parse_args()
 for filename in args.source:
+
     sourcefile = filename
-    print "\n"
-    print 'Ruby source file:', sourcefile
-    source = file(sourcefile,'r')
-    ln = 1
-    while True:
-    	line = source.readline()
-    	if not line: break
-    	print 'ln', ln, ':', line,
-    	ln += 1
-    source.close()
+    if re.search(".rb", sourcefile):
+        print "\n"
+        print 'Ruby source file:', sourcefile
+        source = file(sourcefile,'r')
+        ln = 1
+        while True:
+        	line = source.readline()
+        	if not line: break
+        	print 'ln', ln, ':', line,
+        	ln += 1
+        source.close()
+
+    else:
+        print sourcefile, "is not a valid ruby source"
